e905aab7a89ab9aee3b85b6565a8bb3bf56a0571
sanlurou <oqin39258@gmail.com>
2025-08-21 09:55:33 +0800

    Add files via upload

---
+++ Improved-Masking-for-White-box-AES/build/CMakeFiles/WBAES.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WBAES.dir/test/main.c.obj"
+  "CMakeFiles/WBAES.dir/test/main.c.obj.d"
+  "WBAES.exe"
+  "WBAES.exe.manifest"
+  "WBAES.pdb"
+  "libWBAES.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/WBAES.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Improved-Masking-for-White-box-AES/build/CMakeFiles/WBAES_LIB.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WBAES_LIB.dir/src/WBMatrix/WBMatrix.c.obj"
+  "CMakeFiles/WBAES_LIB.dir/src/WBMatrix/WBMatrix.c.obj.d"
+  "CMakeFiles/WBAES_LIB.dir/src/WBMatrix/random.c.obj"
+  "CMakeFiles/WBAES_LIB.dir/src/WBMatrix/random.c.obj.d"
+  "CMakeFiles/WBAES_LIB.dir/src/aes.c.obj"
+  "CMakeFiles/WBAES_LIB.dir/src/aes.c.obj.d"
+  "CMakeFiles/WBAES_LIB.dir/src/wbaes.c.obj"
+  "CMakeFiles/WBAES_LIB.dir/src/wbaes.c.obj.d"
+  "libWBAES_LIB.a"
+  "libWBAES_LIB.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/WBAES_LIB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
